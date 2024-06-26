8d48b8463eff8a033642c62dc99cb7737a0d2181
Ivan Topchy <nowestclips@gmail.com>
2024-06-26 17:09:18 +0300

    comm

---
+++ test.py
@@ -0,0 +1,18 @@
+def to_upper_case(s):
+    """
+    Convert all characters in the string to uppercase.
+
+    :param s: input string
+    :return: string with all characters in uppercase
+    """
+    return s.upper()
+
+
+def capitalize_words(s):
+    """
+    Capitalize the first letter of each word in the string.
+
+    :param s: input string
+    :return: string with each word capitalized
+    """
+    return s.title()
